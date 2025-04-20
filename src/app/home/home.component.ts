95a53918c2a66a1599c80cc804a1b32d3bcd4772
User <ntuma2022@gmail.com>
2025-04-20 17:16:52 +0400

    dropdown

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,6 +1,6 @@
-import { Component } from '@angular/core';
+import { Component, NgModule } from '@angular/core';
 import { APIsService } from '../services/apis.service';
-import { FormsModule } from '@angular/forms';
+import { FormsModule, NgModel } from '@angular/forms';
 import { Router } from '@angular/router';
 
 @Component({
@@ -23,20 +23,44 @@ export class HomeComponent {
     })
   }
 
+
+
   public whereFrom:string = "";
   public destination:string = "";
   public date:string = "";
   public passengers:number = 1;
 
-  onSubmit(){
-    this.router.navigate(['/რეისები'], {
-      queryParams: {
-        whereFrom: this.whereFrom,
-        destination:this.destination,
-        date:this.date,
-        passengers:this.passengers
-      }
-    })
+  startingStation(name:any){
+    this.whereFrom = name;
   }
+
+  endingStation(name:any){
+    this.destination = name;
+  }
+
+  onSubmit(){
+    console.log(this.whereFrom)
+    console.log(this.destination)
+    console.log(this.date)
+    console.log(this.passengers)
+
+    // this.router.navigate(['/რეისები'], {
+    //   queryParams: {
+    //     whereFrom: this.whereFrom,
+    //     destination:this.destination,
+    //     date:this.date,
+    //     passengers:this.passengers
+    //   }
+    // })
+  }
+
+  reset(){
+    this.whereFrom = "";
+    this.destination = ""
+    this.date = "";
+    this.passengers = 1;
+  }
+
+  
 
 }
